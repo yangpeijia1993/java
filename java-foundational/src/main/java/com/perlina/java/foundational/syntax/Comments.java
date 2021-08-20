44732c0948dcec06e1636e71e5304fbb9a1fdbf4
perlina <759505606@qq.com>
2021-08-21 00:20:23 +0800

    注释的分类和使用

---
+++ java-foundational/src/main/java/com/perlina/java/foundational/syntax/Comments.java
@@ -0,0 +1,35 @@
+package com.perlina.java.foundational.syntax;
+
+/**
+ * 注释的使用
+ *
+ * @author: Perlina
+ * @version:2021/8/2011:43 PM
+ */
+public class Comments {
+    /**
+     * 程序的入口
+     */
+    public static void main(String[] args) {
+        //打印输出
+        System.out.println("描述代码功能");
+
+        /*
+        多行注释
+        Java 应用场景：
+            企业应用开发
+            Android应用开发
+            大数据应用开发
+         */
+
+        /**
+         * 文档注释
+         * 应用场景：
+         *  类上
+         *  方法上
+         */
+    }
+
+
+
+}
