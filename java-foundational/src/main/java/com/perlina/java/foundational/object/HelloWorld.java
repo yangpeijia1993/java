56ffcb539aed5c9e560dba9d31dcb09c38a10c8b
perlina <759505606@qq.com>
2021-08-20 22:59:17 +0800

    我的第一个java程序

---
+++ java-foundational/src/main/java/com/perlina/java/foundational/object/HelloWorld.java
@@ -0,0 +1,13 @@
+package com.perlina.java.foundational.object;
+
+/**
+ * HelloWorld程序
+ *
+ * @author: Perlina
+ * @version:2021/8/2010:57 PM
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World with Mac & JDK8");
+    }
+}
